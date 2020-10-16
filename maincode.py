5e569377ba2ecfc7b06cbfae73cde0e488a77676
Steve-120 <steven.reyes964@gmail.com>
2020-10-16 20:45:18 +0800

    added silog

--- maincode.py
+++ maincode.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
-import os 
+import os
+import random
 
 bot = commands.Bot(command_prefix='=')
 
@@ -11,5 +12,10 @@ async def ping(ctx):
 @bot.command()
 async def lenny(ctx):
 	await ctx.send('( ͡° ͜ʖ ͡°)')
+
+@bot.command()
+async def silog(ctx):
+	silog_list = ['Tapsilog', 'Longsilog', 'Tocilog', 'Bangsilog', 'Cornsilog', 'Hotsilog', 'Porksilog', 'Chicksilog', 'Sisigsilog']
+	await ctx.send(random.choice(silog_list))
 
 bot.run(os.environ['DISCORD_TOKEN'])
