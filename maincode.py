2c3e5cfd23c6de53ac8e3f1d68fdfec1e1b357a1
Steve-120 <steven.reyes964@gmail.com>
2020-10-16 20:05:43 +0800

    setup

---
+++ maincode.py
@@ -0,0 +1,11 @@
+import discord
+from discord.ext import commands
+import os 
+
+bot = commands.Bot(command_prefix='=')
+
+@bot.command()
+async def ping(ctx):
+	await ctx.send('pong')
+
+bot.run(os.environ['DISCORD_TOKEN'])
