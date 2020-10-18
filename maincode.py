88fd63be542a03ee191482b0dd57127ea82ad709
Steve-120 <steven.reyes964@gmail.com>
2020-10-19 00:46:49 +0800

    added feet fetish shamer

--- maincode.py
+++ maincode.py
@@ -25,4 +25,34 @@ async def on_ready():
 	discord_status = discord.Activity(type = discord.ActivityType.watching, name = "if you be anti-PRC")
 	await bot.change_presence(activity = discord_status)
 
+@bot.event
+async def on_message(message):
+	shame_feet(message)
+
 bot.run(os.environ["DISCORD_TOKEN"])
+
+
+def potatilog_only(func):
+	async def wrapper(*args, **kwargs):
+		if args[0].guild == bot.get_guild(698910736117923951):
+			func(*args, **kwargs)
+	return wrapper
+
+@potatilog_only()
+async def shame_feet(message):
+	uncleaned = "ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ　ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ"
+	cleaned = "ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz"
+	cleansor = {}
+	for inp, outp in zip(uncleaned, cleaned):
+		filterer[inp] = outp
+
+	cleaned_msg = ''.join([cleansor[x] if x in cleansor else x for x in ctx.message.content]).lower()
+
+	if 'feet' in cleaned_msg:
+		await message.channel.send('feet fetish sucks')
+'''
+TODO:
+
+on_message 
+if detect "feet/foot": shame foot fetish
+'''
